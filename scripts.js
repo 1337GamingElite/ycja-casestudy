71fe0d2d252738325543c321d8823a7da7ee2bf8
John M <1337gamingelite@gmail.com>
2018-03-07 18:28:29 -0700

    Added google maps for added immersion

--- scripts.js
+++ scripts.js
@@ -1,17 +1,20 @@
 // JavaScript Document
-$(document).ready(function() {
-	$('.nava').click(function(e) {
+$(document).ready(function () {
+	$('.nava').click(function (e) {
 		var linkTo = $(this).attr("href");
 		//console.log(linkTo);
-		
+
 		$('html, body').animate({
 			scrollTop: $(linkTo).offset().top
 		}, 1000);
 	});
-	$(window).on('scroll', function() {
+	$(window).on('scroll', function () {
 		var pixs = $(document).scrollTop();
 		pixs /= 100;
-		$("#landing").css({"-webkit-filter": "blur("+pixs+"px)", "filter": "blur("+pixs+"px)"});
+		$("#landing").css({
+			"-webkit-filter": "blur(" + pixs + "px)",
+			"filter": "blur(" + pixs + "px)"
+		});
 	});
 });
 
@@ -20,7 +23,7 @@ function toggleSituation() {
 	document.getElementById("landing").classList.toggle('active');
 	document.getElementById("landingtext").classList.toggle('active');
 	document.getElementById("main").classList.toggle('active');
-	
+
 	document.getElementById("menubutton").classList.toggle('active');
 }
 
@@ -29,11 +32,90 @@ function toggleNav() {
 	document.getElementById("landing").classList.toggle('menuactive');
 	document.getElementById("landingtext").classList.toggle('menuactive');
 	document.getElementById("main").classList.toggle('menuactive');
+
+	if (document.getElementById("situation").classList.contains("active")) {
+		console.log("h1");
+		document.getElementById("landingtext").classList.toggle("active2");
+	}
+
+}
+
+function initMap() {
+	var center = {
+		lat: 53.537555,
+		lng: -113.435714
+	};
+	var map = new google.maps.Map(document.getElementById("map"), {
+		zoom: 18,
+		center: center,
+		mapTypeId: 'satellite'
+	});
+
+	var causeText = '<h3 class="maptext">The stoplight where it began</h3>';
+
+	var causeWindow = new google.maps.InfoWindow({
+		content: causeText
+	});
+
+	var cause = new google.maps.Marker({
+		position: {
+			lat: 53.537530,
+			lng: -113.436883
+		},
+		map: map,
+		title: 'Where the "street race" started',
+		label: 'A'
+	});
+
+	cause.addListener('click', function () {
+		causeWindow.open(map, cause);
+	});
+
+	var effectText = '<h3 class="maptext">The house that was damaged (now repaired)</h3>';
+
+	var effectWindow = new google.maps.InfoWindow({
+		content: effectText
+	});
+
+	var effect = new google.maps.Marker({
+		position: {
+			lat: 53.537850,
+			lng: -113.435180
+		},
+		map: map,
+		title: 'The house that was damaged',
+		label: 'B'
+	});
+
+	effect.addListener('click', function () {
+		effectWindow.open(map, effect);
+	});
 	
-	if (document.getElementById("situation").classList.contains("active"))
+	var carText = '<h3 class="maptext">Where the car was found severely damaged</h3>';
+	
+	var carWindow = new google.maps.InfoWindow({
+		content: carText
+	});
+	
+	var car = new google.maps.Marker({
+		position: {
+			lat: 53.537730,
+			lng: -113.434984
+		},
+		map: map,
+		title: 'Where the car was found',
+		label: 'C'
+	});
+	
+	car.addListener('click', function () {
+		carWindow.open(map, car);
+	});
+	
+	var markers = [cause, effect, car];
+	var bounds = new google.maps.LatLngBounds();
+	for (var i = 0; i < markers.length; i++)
 		{
-			console.log("h1");
-			document.getElementById("landingtext").classList.toggle("active2");
+			bounds.extend(markers[i].getPosition());
 		}
-	
+	map.fitBounds(bounds);
 }
